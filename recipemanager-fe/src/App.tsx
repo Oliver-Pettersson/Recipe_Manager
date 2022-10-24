907116b6b13d66cd75c59c3eb0b2c7d7df32dda6
Akbar-Noser <Oliver.Pettersson@noseryoung.ch>
2022-10-24 22:56:12 +0200

    added login and signup page

--- recipemanager-fe/src/App.tsx
+++ recipemanager-fe/src/App.tsx
@@ -1,19 +1,24 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "./components/pages/HomePage/HomePage";
 import Sidebar from "./components/organisms/Sidebar/Sidebar";
 import IngredientsPage from "./components/pages/IngredientsPage/IngredientsPage";
 import RecipesPage from "./components/pages/RecipesPage/RecipesPage";
+import LoginPage from "./components/pages/LoginPage/LoginPage";
+import SignUpPage from "./components/pages/SignUpPage/SignUpPage";
 
 function App() {
+  const isAuth = window.location.pathname === "/login" || window.location.pathname === "/signup" 
   return (
-    <div className="App">
+    <div style={isAuth ? {} : {marginLeft: "180px"}} className="App">
       <Router>
-        <Sidebar />
+      {!isAuth && <Sidebar />}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/ingredients" element={<IngredientsPage />} />
           <Route path="/recipes" element={<RecipesPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />}/>
         </Routes>
       </Router>
     </div>
